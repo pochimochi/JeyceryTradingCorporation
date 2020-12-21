ce21879a6885cb2ae0a7c2680a400db62e200ee8
Basco, Carlos Pocholo C <11526642@benilde.edu.ph>
2020-12-21 11:34:25 +0800

    First Commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace JeyceryTradingCorporation
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                    .IncludeDirectory("~/Scripts/", "*.js", true));
+
+
+            
+
+            bundles.Add(new ScriptBundle("~/Content/css")
+                    .IncludeDirectory("~/Content/", "*.css", true));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JeyceryTradingCorporation.MvcApplication" Language="C#" %>
